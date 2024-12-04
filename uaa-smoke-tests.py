a7ee3a925a4c0a89aca8b669aab3bf6c7cc1180d
Van Nguyen <vanh.nguyen@gsa.gov>
2024-12-04 11:25:07 -0500

    Updating smoke tests to reflect new url

--- uaa-smoke-tests.py
+++ uaa-smoke-tests.py
@@ -8,22 +8,19 @@ from bs4 import BeautifulSoup
 
 
 def test_login():
-    resp = requests.get(os.getenv('BASE_URL'))
+    resp = requests.get(os.getenv("BASE_URL"))
 
     # Assert OK
-    assert resp.status_code is 200
+    assert resp.status_code == 200
 
-    page = BeautifulSoup(resp.content)
+    page = BeautifulSoup(resp.content, features="html.parser")
 
     # Assert SAML link with GSA IDP
-    links = page.find_all('a', class_='saml-login-link')
-    next(
-        each for each in links
-        if 'idp=gsa' in each.attrs['href']
-    )
+    links = page.find_all("a", class_="saml-login-link")
+    next(each for each in links if "gsa" in each.attrs["href"])
 
-    assert len(page.find_all('input', class_='island-button')) == 1
-    assert len(page.find_all('div', class_='modal-body')) == 1
+    assert len(page.find_all("input", class_="island-button")) == 1
+    assert len(page.find_all("div", class_="modal-body")) == 1
 
 
 if __name__ == "__main__":
